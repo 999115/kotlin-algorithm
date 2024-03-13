87370298c3eb4321f358fb578348fdda20e09505
999115 <161268524+999115@users.noreply.github.com>
2024-03-13 11:49:14 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer = 0
+        var value = 0
+        
+        for (i : Int in 1..n){
+            if(n%i==0){
+                answer += i
+            }
+        }
+        
+        
+        return answer
+    }
+}
