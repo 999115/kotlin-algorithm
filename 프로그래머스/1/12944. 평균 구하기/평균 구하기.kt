6f7dbe6c6389d0ba348dc69e055dbdb5c2b67969
999115 <161268524+999115@users.noreply.github.com>
2024-03-13 11:48:58 +0900

    [level 1] Title: 평균 구하기, Time: 21.71 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var answer = 0.00
+        fun solution() {answer = arr.average()}
+        solution()
+        return answer
+    }
+}
