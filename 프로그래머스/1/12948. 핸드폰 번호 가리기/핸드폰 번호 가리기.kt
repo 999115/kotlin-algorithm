b6df2d601df67547acc2ee851320bc60df3d15ed
999115 <161268524+999115@users.noreply.github.com>
2024-03-14 11:14:29 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 22.07 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun solution(phone_number: String): String {
         var answer = ""
-        var star = Array(phone_number.length-4) {"*"}
-        answer = star.toList().joinToString("") + phone_number.takeLast(4)
+        var star = List(phone_number.length-4) {"*"}
+        answer = star.joinToString("") + phone_number.takeLast(4)
         
         // for (i in 0..numList.size - 5) {
         //     numList = numList.replace(numList[i], '*')
